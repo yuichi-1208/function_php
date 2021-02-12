beb6df53ce40934747cee33d0455390757fd7653
yuichi oyama <yuichi19991208@gmail.com>
2021-02-12 22:59:12 +0900

    practice sprintf and printf

--- main.php
+++ main.php
@@ -5,3 +5,12 @@ $score = 32.246;
 
 $info = "[$name][$score]";
 echo $info . PHP_EOL;
+
+$info = sprintf("[%15s][%10.2f]", $name, $score);
+echo $info . PHP_EOL;
+
+$info = sprintf("[%-15s][%010.2f]", $name, $score);
+echo $info . PHP_EOL;
+
+printf("[%-15s][%010.2f]" . PHP_EOL , $name, $score);
+echo $info . PHP_EOL;
