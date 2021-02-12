019bc107b808e59014372db04f3c56b873e845ea
yuichi oyama <yuichi19991208@gmail.com>
2021-02-12 23:26:41 +0900

    正規表現での文字列の検索や置換

--- main.php
+++ main.php
@@ -31,15 +31,27 @@
 // $input = str_replace('にち', 'ばん', $input);
 // echo $input . PHP_EOL;
 
-$input = '20200320Item-A  1200';
-$input = substr_replace($input, 'Item-B  ', 8, 8);
+// $input = '20200320Item-A  1200';
+// $input = substr_replace($input, 'Item-B  ', 8, 8);
 
-$date = substr($input, 0, 8);
-$product = substr($input, 8, 8);
-// $amount = substr($input, 16, 4);
-$amount = substr($input, 16);
+// $date = substr($input, 0, 8);
+// $product = substr($input, 8, 8);
+// // $amount = substr($input, 16, 4);
+// $amount = substr($input, 16);
 
-echo $date . PHP_EOL;
-echo $product . PHP_EOL;
-// echo $amount . PHP_EOL;
-echo number_format($amount) . PHP_EOL;
+// echo $date . PHP_EOL;
+// echo $product . PHP_EOL;
+// // echo $amount . PHP_EOL;
+// echo number_format($amount) . PHP_EOL;
+
+
+$input = 'Call us at 03-3001-1256 or 03-3015-3222';
+$pattern = '/\d{2}-\d{4}-\d{4}/';
+
+preg_match($pattern, $input, $matches);
+print_r($matches);
+preg_match_all($pattern, $input, $matches);
+print_r($matches);
+
+$input = preg_replace($pattern, '**-****-****', $input);
+echo $input . PHP_EOL;
