810528225a359d89aa70eb922c91f0a28cbdde31
yuichi oyama <yuichi19991208@gmail.com>
2021-02-12 23:06:43 +0900

    文字数表示、空白削除、文字検索、文字置換

--- main.php
+++ main.php
@@ -1,16 +1,24 @@
 <?php
 
-$name = "Apple";
-$score = 32.246;
+// $name = "Apple";
+// $score = 32.246;
 
-$info = "[$name][$score]";
-echo $info . PHP_EOL;
+// $info = "[$name][$score]";
+// echo $info . PHP_EOL;
 
-$info = sprintf("[%15s][%10.2f]", $name, $score);
-echo $info . PHP_EOL;
+// $info = sprintf("[%15s][%10.2f]", $name, $score);
+// echo $info . PHP_EOL;
 
-$info = sprintf("[%-15s][%010.2f]", $name, $score);
-echo $info . PHP_EOL;
+// $info = sprintf("[%-15s][%010.2f]", $name, $score);
+// echo $info . PHP_EOL;
 
-printf("[%-15s][%010.2f]" . PHP_EOL , $name, $score);
-echo $info . PHP_EOL;
+// printf("[%-15s][%010.2f]" . PHP_EOL , $name, $score);
+// echo $info . PHP_EOL;
+
+$input = ' dot_taguchi  ';
+$input = trim($input);
+echo strlen($input) . PHP_EOL;
+echo strpos($input, '_') . PHP_EOL;
+
+$input = str_replace('_', '-', $input);
+echo $input . PHP_EOL;
