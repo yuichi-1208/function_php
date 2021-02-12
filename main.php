d2d4d07f7424bbda94f31282db8868d32ae6ab5a
yuichi oyama <yuichi19991208@gmail.com>
2021-02-12 23:18:06 +0900

    固定長データの取り扱い

--- main.php
+++ main.php
@@ -23,10 +23,23 @@
 // $input = str_replace('_', '-', $input);
 // echo $input . PHP_EOL;
 
-$input = ' こんにちは  ';
-$input = trim($input);
-echo mb_strlen($input) . PHP_EOL;
-echo mb_strpos($input, 'に') . PHP_EOL;
+// $input = ' こんにちは  ';
+// $input = trim($input);
+// echo mb_strlen($input) . PHP_EOL;
+// echo mb_strpos($input, 'に') . PHP_EOL;
 
-$input = str_replace('にち', 'ばん', $input);
-echo $input . PHP_EOL;
+// $input = str_replace('にち', 'ばん', $input);
+// echo $input . PHP_EOL;
+
+$input = '20200320Item-A  1200';
+$input = substr_replace($input, 'Item-B  ', 8, 8);
+
+$date = substr($input, 0, 8);
+$product = substr($input, 8, 8);
+// $amount = substr($input, 16, 4);
+$amount = substr($input, 16);
+
+echo $date . PHP_EOL;
+echo $product . PHP_EOL;
+// echo $amount . PHP_EOL;
+echo number_format($amount) . PHP_EOL;
