3e1ab1a3023f2915d33f2a70cb82316575526879
yuichi oyama <yuichi19991208@gmail.com>
2021-02-14 00:08:03 +0900

    ファイルの内容をまとめて読み込む

--- main.php
+++ main.php
@@ -288,13 +288,24 @@
 // fclose($fp);
 
 
-$fp = fopen('names.txt', 'r');
-$contents = fread($fp, filesize('names.txt'));
-fclose($fp);
-echo $contents;
+// $fp = fopen('names.txt', 'r');
+// $contents = fread($fp, filesize('names.txt'));
+// fclose($fp);
+// echo $contents;
 
-$fp = fopen('names.txt', 'r');
-while (($line = fgets($fp)) !== false) {
-  echo $line;
-}
-fclose($fp);
+// $fp = fopen('names.txt', 'r');
+// while (($line = fgets($fp)) !== false) {
+//   echo $line;
+// }
+// fclose($fp);
+
+
+
+// $contents = "taro\njiro\nsaburo\n";
+// file_put_contents('firstname.txt', $contents);
+
+// $contents = file_get_contents('firstname.txt');
+// echo $contents;
+
+$lines = file('firstname.txt', FILE_IGNORE_NEW_LINES);
+var_dump($lines);
