23dfa2f85fd370297eb8ac9bf3b02aadebf63ead
yuichi oyama <yuichi19991208@gmail.com>
2021-02-14 00:20:06 +0900

    ディレクトリの操作

--- main.php
+++ main.php
@@ -307,5 +307,23 @@
 // $contents = file_get_contents('firstname.txt');
 // echo $contents;
 
-$lines = file('firstname.txt', FILE_IGNORE_NEW_LINES);
-var_dump($lines);
+// $lines = file('firstname.txt', FILE_IGNORE_NEW_LINES);
+// var_dump($lines);
+
+
+
+file_put_contents('data/taro.txt', "taro\n");
+file_put_contents('data/jiro.txt', "jiro\n");
+
+$dp = opendir('data');
+while (($item = readdir($dp)) !== false) {
+  if ($item === '.' || $item === '..') {
+    continue;
+  }
+  echo $item . PHP_EOL;
+}
+
+foreach (glob('data/*.txt') as $item) {
+  // echo $item . PHP_EOL;
+  echo basename($item) . PHP_EOL;
+}
